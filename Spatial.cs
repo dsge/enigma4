84115d43e43c9e7336439393d9770f64f3f1b7e0
dsge <gerx03@gmail.com>
2019-06-04 20:44:53 +0200

    make the cube spin

--- Spatial.cs
+++ Spatial.cs
@@ -10,12 +10,21 @@ public class Spatial : Godot.Spatial
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("This is the same as overriding _Ready()...");  
+        GD.Print("This is the same as overriding _Ready()... 2");
+
+        SurfaceTool surfaceTool = new SurfaceTool();
+        surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+        surfaceTool.AddVertex(new Vector3(0f, 0f, 1f));
+        surfaceTool.AddVertex(new Vector3(0f, 1f, 0f));
+        surfaceTool.AddVertex(new Vector3(1f, 0f, 0f));
+        ArrayMesh arrayMesh = surfaceTool.Commit();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Process(float delta)
+    {
+        Godot.Spatial cube = GetNode<Godot.Spatial>("../Cube");
+
+        cube.Rotation = new Vector3(cube.Rotation.x + delta * 2, cube.Rotation.y, cube.Rotation.z);
+    }
 }
