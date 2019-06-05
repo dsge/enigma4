4f06c6e4fda204086a37cbc8db5e136f763c7481
dsge <gerx03@gmail.com>
2019-06-05 22:23:01 +0200

    show a plane

--- Spatial.cs
+++ Spatial.cs
@@ -13,11 +13,21 @@ public class Spatial : Godot.Spatial
         GD.Print("This is the same as overriding _Ready()... 2");
 
         SurfaceTool surfaceTool = new SurfaceTool();
-        surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
-        surfaceTool.AddVertex(new Vector3(0f, 0f, 1f));
-        surfaceTool.AddVertex(new Vector3(0f, 1f, 0f));
+        surfaceTool.Begin(Mesh.PrimitiveType.TriangleFan);
         surfaceTool.AddVertex(new Vector3(1f, 0f, 0f));
+        surfaceTool.AddVertex(new Vector3(1f, 0f, 1f));
+        surfaceTool.AddVertex(new Vector3(0f, 0f, 1f));
+        surfaceTool.AddVertex(new Vector3(0f, 0f, 0f));
         ArrayMesh arrayMesh = surfaceTool.Commit();
+
+        var tmpMesh = new MeshInstance();
+        var mat = new SpatialMaterial();
+        var color = new Color(0.9f, 0.1f, 0.1f);
+        mat.AlbedoColor = color;
+        tmpMesh.Mesh = arrayMesh;
+        tmpMesh.Scale = new Vector3(5f, 5f, 5f);
+
+        AddChild(tmpMesh);
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
