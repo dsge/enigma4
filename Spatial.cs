09f0f16a2a8fc7144c5696fc04c7f9590c513d62
dsge <gerx03@gmail.com>
2019-06-15 20:01:22 +0200

    write "hit" or "miss" to console depending on clicking the floor or not

--- Spatial.cs
+++ Spatial.cs
@@ -1,12 +1,12 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public class Spatial : Godot.Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
+    protected Vector3 raycastFrom = Vector3.Zero;
+    protected Vector3 raycastTo = Vector3.Zero;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -27,13 +27,43 @@ public class Spatial : Godot.Spatial
         cube.Rotation = new Vector3(cube.Rotation.x + delta * 2, cube.Rotation.y, cube.Rotation.z);
     }
 
+    public override void _Input(InputEvent @event){
+        if (@event is InputEventMouseButton eventMouseButton) {
+            if (eventMouseButton.ButtonIndex == 1 && eventMouseButton.Pressed) {
+                var rayLength = 1000;
+                var camera = (Camera)GetNode("Camera");
+                this.raycastFrom = camera.ProjectRayOrigin(eventMouseButton.Position);
+                this.raycastTo = raycastFrom + camera.ProjectRayNormal(eventMouseButton.Position) * rayLength;
+            }
+        }
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        if (!this.raycastFrom.Equals(Vector3.Zero) && !this.raycastFrom.Equals(Vector3.Zero)) {
+            var spaceState = GetWorld().DirectSpaceState;
+            Dictionary result = spaceState.IntersectRay(this.raycastFrom, this.raycastTo);
+            if (result.Count > 0) {
+                var obj = (Godot.StaticBody)result["collider"];
+                var obj2 = obj.GetNode<Godot.Spatial>("..");
+                GD.Print("hit");
+            } else {
+                GD.Print("miss");
+            }
+            this.raycastFrom = Vector3.Zero;
+            this.raycastTo = Vector3.Zero;
+        }
+    }
+
     protected MeshInstance createFloor() {
         SurfaceTool surfaceTool = new SurfaceTool();
-        surfaceTool.Begin(Mesh.PrimitiveType.TriangleFan);
+        surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
         surfaceTool.AddVertex(new Vector3(1f, 0f, 0f));
         surfaceTool.AddVertex(new Vector3(1f, 0f, 1f));
         surfaceTool.AddVertex(new Vector3(0f, 0f, 1f));
+        surfaceTool.AddVertex(new Vector3(0f, 0f, 1f));
         surfaceTool.AddVertex(new Vector3(0f, 0f, 0f));
+        surfaceTool.AddVertex(new Vector3(1f, 0f, 0f));
         ArrayMesh arrayMesh = surfaceTool.Commit();
 
         var ret = new MeshInstance();
@@ -43,6 +73,10 @@ public class Spatial : Godot.Spatial
         ret.Mesh = arrayMesh;
         ret.Scale = new Vector3(15f, 15f, 15f);
         ret.Translation = new Vector3(-7.5f, 0, -7.5f);
+
+
+        ret.CreateConvexCollision();
+
         return ret;
     }
 }
