8733a6c23275723c4afae700c84042d31eec8e66
dsge <gerx03@gmail.com>
2019-07-04 23:42:24 +0200

    add better way to move around

--- Main.cs
+++ Main.cs
@@ -8,23 +8,19 @@ public class Main : Godot.Spatial
     protected Vector3 raycastFrom = Vector3.Zero;
     protected Vector3 raycastTo = Vector3.Zero;
     // Called when the node enters the scene tree for the first time.
-    protected Navigation navigation = new Navigation();
-    protected KinematicBody playerCharacter;
-    protected string mainCamera = "PlayerCharacter/Camera";
+    protected Navigation navigation = null;
+    protected KinematicBody playerCharacter = null;
+    protected Camera mainCamera = null;
+    protected Vector3[] playerMovementPath;
     public override void _Ready()
     {
         GD.Print("This is the same as overriding _Ready()... 2");
 
-        /* AddChild(this.createFloor());
-        this.playerCharacter = this.createPlayerCharacter();
-        AddChild(this.playerCharacter);*/
-
-        /*Godot.Spatial camera = GetNode<Godot.Spatial>("Camera");
-        camera.Translation = new Vector3(10f, 10f, 10f);
-        camera.LookAt(new Vector3(0, 0, 0), new Vector3(0, 1f, 0));*/
+        this.mainCamera = (Camera)GetNode("Navigation/PlayerCharacter/Camera");
+        this.playerCharacter = (KinematicBody)GetNode("Navigation/PlayerCharacter");
+        this.navigation = (Navigation)GetNode("Navigation");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
         Godot.Spatial cube = GetNode<Godot.Spatial>("Cube");
@@ -36,7 +32,7 @@ public class Main : Godot.Spatial
         if (@event is InputEventMouseButton eventMouseButton) {
             if (eventMouseButton.ButtonIndex == 1 && eventMouseButton.Pressed) {
                 var rayLength = 1000;
-                var camera = (Camera)GetNode(mainCamera);
+                var camera = this.mainCamera;
                 this.raycastFrom = camera.ProjectRayOrigin(eventMouseButton.Position);
                 this.raycastTo = raycastFrom + camera.ProjectRayNormal(eventMouseButton.Position) * rayLength;
             }
@@ -58,14 +54,14 @@ public class Main : Godot.Spatial
                     }
                 }
 
-                if (obj is StaticBody staticBody && this.playerCharacter != null) {
+                if (obj is StaticBody staticBody && this.playerCharacter != null && this.navigation != null) {
                     Vector3 position = staticBody.ToGlobal((Vector3)result["position"]);
                     var path = this.navigation.GetSimplePath(this.playerCharacter.Translation, position);
                     if (path.Length > 0) {
                         if (this.playerCharacter.IsOnFloor()) {
                             // this.playerCharacter.MoveLockY = true;
                         }
-                        this.playerCharacter.MoveAndSlide(this.playerCharacter.ToLocal(path[1]) * 60);
+                        this.playerMovementPath = path;
                     }
                 }
 
@@ -76,78 +72,15 @@ public class Main : Godot.Spatial
             this.raycastFrom = Vector3.Zero;
             this.raycastTo = Vector3.Zero;
         }
-    }
 
-    protected Godot.Spatial createFloor() {
-        SurfaceTool surfaceTool = new SurfaceTool();
-        surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
-        surfaceTool.AddVertex(new Vector3(15f, 0f, 0f));
-        surfaceTool.AddVertex(new Vector3(15f, 0f, 15f));
-        surfaceTool.AddVertex(new Vector3(0f, 0f, 15f));
-        surfaceTool.AddVertex(new Vector3(0f, 0f, 15f));
-        surfaceTool.AddVertex(new Vector3(0f, 0f, 0f));
-        surfaceTool.AddVertex(new Vector3(15f, 0f, 0f));
-        surfaceTool.AddIndex(0);
-        surfaceTool.AddIndex(1);
-        surfaceTool.AddIndex(2);
-        surfaceTool.AddIndex(3);
-        surfaceTool.AddIndex(4);
-        surfaceTool.AddIndex(5);
-        ArrayMesh arrayMesh = surfaceTool.Commit();
-
-        var meshInstance = new MeshInstance();
-        var mat = new SpatialMaterial();
-        var color = new Color(0.9f, 0.1f, 0.1f);
-        mat.AlbedoColor = color;
-        meshInstance.Mesh = arrayMesh;
-        meshInstance.Translation = new Vector3(-7.5f, 0, -7.5f);
-
-        StaticBody rigidBody = new StaticBody();
-        // rigidBody.Mode = RigidBody.ModeEnum.Static;
-
-        BoxShape boxShape = new BoxShape();
-        boxShape.Extents = new Vector3(7.5f, 0.0001f, 7.5f);
-
-        CollisionShape collisionShape = new CollisionShape();
-        collisionShape.Shape = boxShape;
-
-        rigidBody.AddChild(collisionShape);
-        rigidBody.AddChild(meshInstance);
-
-        Godot.Spatial ret = new Godot.Spatial();
-
-        ret.AddChild(rigidBody);
-        //ret.Translation = new Vector3(-7.5f, 0, -7.5f);
-
-        NavigationMesh navMesh = new NavigationMesh();
-        navMesh.CreateFromMesh(meshInstance.Mesh);
-
-        this.navigation.NavmeshAdd(navMesh, meshInstance.Transform);
-
-        return ret;
-    }
-
-    protected KinematicBody createPlayerCharacter() {
-        var packedScene = GD.Load<PackedScene>("res://gameObjects/Cylinder.tscn");
-
-        MeshInstance ret = (MeshInstance)packedScene.Instance();
-        ret.Translation = new Vector3(0, 0, 0);
-
-        KinematicBody rigidBody = new KinematicBody();
-
-        CylinderShape shape = new CylinderShape();
-        shape.Radius = 1f;
-        shape.Height = 2f;
-
-        CollisionShape collisionShape = new CollisionShape();
-        collisionShape.Shape = shape;
-
-        rigidBody.AddChild(collisionShape);
-
-        rigidBody.AddChild(ret);
-
-        rigidBody.Translation = new Vector3(0, 0.6f + 5f, 0);
-
-        return rigidBody;
+        if (this.playerMovementPath != null && this.playerCharacter != null) {
+            Vector3 targetPoint = this.playerMovementPath[1];
+            var speed = 10f;
+            this.playerCharacter.MoveAndCollide(this.playerCharacter.ToLocal(targetPoint).Normalized() * speed * delta);
+            var distance = this.playerCharacter.Translation.DistanceTo(targetPoint);
+            if (distance < 1f) {
+                this.playerMovementPath = null;
+            }
+        }
     }
 }
