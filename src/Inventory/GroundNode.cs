2729f76ef76be976ad4eff6e1cfb099b5f402453
dsge <gerx03@gmail.com>
2019-09-08 16:40:44 +0200

    add a hover effect to GroundNode

--- src/Inventory/GroundNode.cs
+++ src/Inventory/GroundNode.cs
@@ -14,17 +14,34 @@ namespace App.Inventory
 
         public GroundNode(MeshInstance meshInstance) : base() {
             this.meshInstance = meshInstance;
+            Godot.Collections.Array children = meshInstance.GetChildren();
+            foreach (var child in children) {
+                if (child is CollisionObject collider) {
+                    //collider.Connect("input_event", this, nameof(this.foo));
+                    collider.Connect("mouse_entered", this, nameof(this.onMouseEntered));
+                    collider.Connect("mouse_exited", this, nameof(this.onMouseExited));
+                    break;
+                }
+            }
+            // meshInstance.MaterialOverride = new
         }
-        public override void _Ready() {
-            this.AddChild(this.meshInstance);
+
+        public void foo(Node camera, InputEvent e, Vector3 click_position, Vector3 click_normal, int shape_idx) {
+            GD.Print("AAYYYYYYY");
         }
 
         public void onMouseEntered() {
-            this.shouldHaveHoverEffects = true;
+            var mat = new ShaderMaterial();
+            mat.Shader = (Shader)GD.Load("res://scenes/item-on-ground-hover.shader");
+            this.meshInstance.MaterialOverride = mat;
         }
 
         public void onMouseExited() {
-            this.shouldHaveHoverEffects = false;
+            this.meshInstance.MaterialOverride = null;
+        }
+
+        public override void _Ready() {
+            this.AddChild(this.meshInstance);
         }
     }
 }
