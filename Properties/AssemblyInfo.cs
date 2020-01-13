76f42cf884a7e7c269aa3f86b157ee81cbd682e3
dsge <gerx03@gmail.com>
2020-01-13 08:00:56 +0100

    enigma3 -> enigma4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("enigma3")]
+[assembly: AssemblyTitle("enigma4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
