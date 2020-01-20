feb6026de056451c40d065792520be97ad6535df
dsge <gerx03@gmail.com>
2020-01-20 11:33:16 +0100

    add cockpitblock hover highlight

--- src/App/Grid/CockpitBlock.cs
+++ src/App/Grid/CockpitBlock.cs
@@ -4,18 +4,33 @@ namespace App.Grid
 {
     public class CockpitBlock: GridCell {
         protected new Vector3 size = new Vector3(2f, 1f, 1f);
-        protected PackedScene prototype = (PackedScene)GD.Load("res://gameObjects/Grid/CockpitBlock.tscn");
+        protected PackedScene prototype = (PackedScene)GD.Load("res://gameObjects/Grid/Cockpit.tscn");
+        protected Node instance = null;
+
         public override void _Ready() {
-            var instance = prototype.Instance();
-            this.AddChild(instance);
+            this.instance = prototype.Instance();
+            this.AddChild(this.instance);
+
+            this.FindNode("window", true, false).FindNode("StaticBody", true, false).Connect("mouse_entered", this, "onCockpitWindowMouseEntered");
+            this.FindNode("window", true, false).FindNode("StaticBody", true, false).Connect("mouse_exited", this, "onCockpitWindowMouseExited");
         }
 
-        public void activateManualThrust() {
-            this.FindNode("MeshInstance2", false);
+        protected void onCockpitWindowMouseEntered() {
+            this.setWindowHighlight(true);
         }
 
-        public void deactivateManualThrust() {
+        protected void onCockpitWindowMouseExited() {
+            this.setWindowHighlight(false);
+        }
 
+        protected void setWindowHighlight(bool value) {
+            AnimationPlayer animationPlayer = (AnimationPlayer)this.instance.FindNode("AnimationPlayer", true, false);
+
+            if (value) {
+                animationPlayer.Play("windowhighlight");
+            } else {
+                animationPlayer.PlayBackwards("windowhighlight");
+            }
         }
     }
 }
